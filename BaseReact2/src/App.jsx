fd55a2c14bccad3340d4ded5b0f9c0f74df56ebf
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-07-28 22:28:28 -0300

    feat: botão de toggleComplete feito.

--- BaseReact2/src/App.jsx
+++ BaseReact2/src/App.jsx
@@ -1,34 +1,47 @@
 import { useState } from "react";
-import { EyeOff, Eye } from "lucide-react";
 
 function App() {
-  const [userName, setUserName] = useState("");
-  const [passWordVisible, setPasswordVisible] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Aprender React",
+      Completed: false,
+      tags: ["estudo", "frontend"],
+    },
+    {
+      id: 2,
+      text: "Fazer compras",
+      Completed: false,
+      tags: ["casa"],
+    },
+  ]);
+
+  const togleCompleted = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          Completed: !task.Completed,
+        };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  };
 
   return (
     <div>
       <div>
-        <input
-          type="text"
-          placeholder="digite o nome de usuario"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-
-        <input
-          type={passWordVisible ? "text" : "password"}
-          placeholder="Digite a senha"
-        />
-
-        <button
-          onClick={() => {
-            setPasswordVisible((pass) => !pass);
-          }}
-        >
-          {passWordVisible ? <Eye /> : <EyeOff />}
-        </button>
+        <input type="text" placeholder="Digite o texto da tarefa " />
+        <button>Adicionar tarefa</button>
       </div>
-      <h2>Bem vindo {userName}</h2>
+
+      <div>
+        {tasks.map((task) => {
+          const key = task.id;
+          return <p>{task.text}</p>;
+        })}
+      </div>
     </div>
   );
 }
